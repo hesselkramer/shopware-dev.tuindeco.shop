b9d5a15c3ce110b86be6eb82a8090ddbb586275b
Hessel Kramer <hessel@hesselkramer.nl>
2020-11-14 20:05:01 +0100

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,14 +2,14 @@
 sed -i 's/Options Indexes FollowSymLinks/Options FollowSymLinks/g'  /etc/httpd/conf/httpd.conf
 service httpd restart
 cd /
-cd /var/www/laravel
+cd /var/www/html
 composer install -n
-chown -R apache.apache /var/www/laravel
-chmod -R 755 /var/www/laravel
-chmod -R 755 /var/www/laravel/storage
+chown -R apache.apache /var/www/html
+chmod -R 755 /var/www/html
+chmod -R 755 /var/www/html/storage
 cp -n .env.example .env
 php artisan key:generate
 cd /
 cd /etc/httpd/conf/
-sed -i 's|DocumentRoot "/var/www/html"|DocumentRoot /var/www/laravel/public|g' httpd.conf
+sed -i 's|DocumentRoot "/var/www/html"|DocumentRoot /var/www/html/public|g' httpd.conf
 service httpd restart
