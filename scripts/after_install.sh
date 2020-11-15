54e0eb3ac4c02743c817a3f5469759ff8e74e784
Hessel Kramer <hessel@hesselkramer.nl>
2020-11-15 07:25:57 +0100

    update

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,12 +3,12 @@ sed -i 's/Options Indexes FollowSymLinks/Options FollowSymLinks/g'  /etc/httpd/c
 service httpd restart
 cd /
 cd /var/www/html
-composer install -n
+#composer install -n
 chown -R apache.apache /var/www/html
 chmod -R 755 /var/www/html
 chmod -R 755 /var/www/html/storage
-cp -n .env.example .env
-php artisan key:generate
+#cp -n .env.example .env
+#php artisan key:generate
 cd /
 cd /etc/httpd/conf/
 sed -i 's|DocumentRoot "/var/www/html"|DocumentRoot /var/www/html/public|g' httpd.conf
